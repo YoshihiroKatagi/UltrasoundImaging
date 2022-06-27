f7f32bbabc4a3e3f78ac730fd957b7cab5bf8707
YoshihiroKatagi <katagiyoshihiro@gmail.com>
2022-06-27 17:58:58 +0900

    completed LinearRegressionForEachDataVer2.py

--- OpticalFlow/direct/LinearRegressionForEachDataVer2.py
+++ OpticalFlow/direct/LinearRegressionForEachDataVer2.py
@@ -3,7 +3,8 @@
 # Ver1: Wが間違っている(179, 100) 時系列は関係ないはず
 # Ver2: Wを修正(1, 100) そのためにAnalysis()を修正
 # Ver2: 訓練データを含む全データでテストを行うX_test = X
-# Ver2: 12パターンでループ、データシートを考慮
+# Ver2: 12パターンでループ
+# Ver2: 
 
 
 import numpy as np
@@ -22,19 +23,24 @@ target_date = "2022-05-30"
 
 target_path = "C:/Users/katagi/Desktop/Workspace/Research/UltrasoundImaging/OpticalFlow/direct/dataset/" + target_date + "/forMachineLearning/"
 
-# 検証するパターン（12パターン × 10試行 = 120個）
-patern = 1 # 1~12(1, 2, 3, 4, 8, 11, 12)
+# 検証するパターン数（12パターン × 10試行 = 120個）
+patern_num = 12
+#####################################################################
 
+#######################  Make Results Path  #########################
 # 結果保存用パス
-result_folder = "dataset/" + target_date + "/results"
-result_path = result_folder + "/patern" + str(patern)
+def MakeResultsPath(patern):
+  result_folder = "dataset/" + target_date + "/results"
+  result_path = result_folder + "/patern" + str(patern)
 
-if not os.path.exists(result_path):
-  os.makedirs(result_path)
+  if not os.path.exists(result_path):
+    os.makedirs(result_path)
 
-with open(result_path + "/R2andRMSE.csv", "w") as f:
-  writer = csv.writer(f)
-  writer.writerow(["RMSE", "R2"])
+  with open(result_path + "/R2andRMSE.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["RMSE", "R2"])
+  
+  return result_path
 #####################################################################
 
 ###########################  Read Data  #############################
@@ -50,17 +56,6 @@ def ReadData(): # 10試行分（どのパターンかは上のパラメータで
 
   return feature_points_data, gonio_data
 #####################################################################
-
-# ###################  Divide into Train and Test  ####################
-# def DivideIntoTrainAndTest(x, theta):# Train:(4, 179, 100), (4, 179, 1)  Test:(179, 100), (179, 1)
-#   x, theta = x[3:], theta[3:]
-#   x = x.reshape(5, x.shape[0] // 5, x.shape[1])
-#   theta = theta.reshape(5, theta.shape[0] // 5, theta.shape[1])
-#   x_train, theta_train = x[:4], theta[:4]
-#   x_test, theta_test = x[4], theta[4]
-
-#   return x_train, theta_train, x_test, theta_test
-# #####################################################################
 
 ###################  Divide into Train and Test  ####################
 def DivideIntoTrainAndTest(x, theta):# Train:(718, 100), (718, 1)  Test:(898, 100), (898, 1)
@@ -82,7 +77,8 @@ def Analysis(x, theta): #x: (718, 100), theta: (718, 1)
 ###########################  Visualize  #############################
 def Visualize(y, y_pred, n):
   x = np.arange(y.shape[0])
-  y = y.reshape(-1) # (179,)
+  y = y.reshape(-1) # (898,)
+  y_pred = y_pred.reshape(-1) # (898,)
 
   fig1 = plt.figure()
   plt.title("Wrist angle")
@@ -90,6 +86,7 @@ def Visualize(y, y_pred, n):
   plt.ylabel("angle")
   plt.plot(x, y_pred, color="cornflowerblue", linewidth=2, label="Estimated angle")
   plt.plot(x, y, color="tomato", linewidth=2, label="Measured angle")
+  plt.vlines(718, -80, 30, "gray", linestyles="dashed")
   plt.ylim(-80, 30) #extensor
   plt.xlim(0, y.shape[0])
   plt.legend(loc="upper left", fontsize=10)
@@ -102,11 +99,13 @@ def Visualize(y, y_pred, n):
   fig2.savefig(result_path + "/scatter" + str(n) + ".png")
 
   # plt.show()
+  plt.close()
 #####################################################################
 
 ########################  Calc_R2andRMSE  ###########################
 def Calc_R2andRMSE(theta, theta_pred, T):
   theta = theta.reshape(-1)
+  theta_pred = theta_pred.reshape(-1)
 
   def _R2(theta, theta_pred):
     corrcoef = np.corrcoef(theta, theta_pred)
@@ -129,22 +128,23 @@ def Calc_R2andRMSE(theta, theta_pred, T):
 #####################################################################
 
 #############################  Main  ################################
-print("-----Patern" + str(patern) + "-----\n")
+for p in range(patern_num):
+  patern = p + 1
+  result_path = MakeResultsPath(patern)
+  print("-----Patern" + str(patern) + "-----\n")
 
-Xs, Thetas = ReadData() # (10, 898, 100), (10, 898, 1)
-T = Xs.shape[1]
-for i in range(10):
-  test_num = i + 1
-  print("--test" + str(test_num) + "--")
+  Xs, Thetas = ReadData() # (10, 898, 100), (10, 898, 1)
+  T = Xs.shape[1]
+  for i in range(10):
+    test_num = i + 1
+    print("--test" + str(test_num) + "--")
 
-  X, Theta = Xs[i], Thetas[i] # (898, 100), (898, 1)
-  X_train, Theta_train, X_test, Theta_test = DivideIntoTrainAndTest(X, Theta) # (718, 100), (718, 1), (898, 100), (898, 1)
-  W = Analysis(X_train, Theta_train) # (1, 100)
+    X, Theta = Xs[i], Thetas[i] # (898, 100), (898, 1)
+    X_train, Theta_train, X_test, Theta_test = DivideIntoTrainAndTest(X, Theta) # (718, 100), (718, 1), (898, 100), (898, 1)
+    W = Analysis(X_train, Theta_train) # (100, 1)
 
-  Theta_pred = list()
-  for X_t, W_t in zip(X_test, W):
-    Theta_pred.append(np.dot(W_t, X_t))
-  Theta_pred = np.array(Theta_pred) # (179,)
-  Visualize(Theta_test, Theta_pred, test_num)
-  Calc_R2andRMSE(Theta_test, Theta_pred, T)
+    Theta_pred = np.dot(X_test, W) # (898, 1)
+
+    Visualize(Theta_test, Theta_pred, test_num)
+    Calc_R2andRMSE(Theta_test, Theta_pred, T)
 #####################################################################
